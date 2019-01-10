d02e8c79603a4677396a0e5586af36897fc5c5ac
Michał Cop <40185529+Findaa@users.noreply.github.com>
2019-01-10 15:50:35 +0100

    Update AddPostActivity.java

--- main/java/com/example/michalcop/rememberall/activity/AddPostActivity.java
+++ main/java/com/example/michalcop/rememberall/activity/AddPostActivity.java
@@ -31,19 +31,22 @@ import java.util.Date;
  * Activity that allows us to add custom elements to room database.
  */
 public class AddPostActivity extends AppCompatActivity implements View.OnClickListener {
+    
     //Constants for intent names.
     public static final String EXTRA_TITLE = "com.findaa.TITLE";
     public static final String EXTRA_CONTENT = "com.findaa.CONTENT";
     public static final String EXTRA_DATEFOR = "com.findaa.DATEFOR";
+    
     //Inject view model.
     PostViewModel postViewModel;
+    
     //Create button animation.
     private AlphaAnimation buttonClick = new AlphaAnimation(1F, 0.4F);
 
+    //create variables
     Button pickDate, pickTime;
     int mYear, mMonth, mDay, mHour, mMinute;
-
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,78 +57,72 @@ public class AddPostActivity extends AppCompatActivity implements View.OnClickLi
         pickTime = (Button) findViewById(R.id.pickTime);
         pickDate.setOnClickListener(this);
         pickTime.setOnClickListener(this);
-
-
     }
 
-
     @Override
-    public void onClick(View v){
-       if (v == pickDate){
-           final Calendar c = Calendar.getInstance();
-           mYear = c.get(Calendar.YEAR);
-           mMonth = c.get(Calendar.MONTH);
-           mDay = c.get(Calendar.DAY_OF_MONTH);
-           pickDate.getBackground().setColorFilter(0x77000000, PorterDuff.Mode.SRC_ATOP);
-           TextView dateTxt = (TextView) findViewById(R.id.editDateFor);
+    public void onClick(View v) {
+        if (v == pickDate) {
+            final Calendar c = Calendar.getInstance();
+            mYear = c.get(Calendar.YEAR);
+            mMonth = c.get(Calendar.MONTH);
+            mDay = c.get(Calendar.DAY_OF_MONTH);
+            pickDate.getBackground().setColorFilter(0x77000000, PorterDuff.Mode.SRC_ATOP);
+            TextView dateTxt = (TextView) findViewById(R.id.editDateFor);
 
-           DatePickerDialog datePickerDialog = new DatePickerDialog(this,
-                   (view, year, monthOfYear, dayOfMonth) ->
-                           dateTxt
-                                   .setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year), mYear, mMonth, mDay);
-           datePickerDialog.show();
-
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                    (view, year, monthOfYear, dayOfMonth) ->
+                            dateTxt.setText(
+                                    dayOfMonth + "-" + (monthOfYear + 1) + "-" + year), mYear, mMonth, mDay);
+            
+            datePickerDialog.show();
         }
-        if (v == pickTime){
+        
+        if (v == pickTime) {
             final Calendar c = Calendar.getInstance();
             mHour = c.get(Calendar.HOUR_OF_DAY);
             mMinute = c.get(Calendar.MINUTE);
             pickTime.getBackground().setColorFilter(0x77000000, PorterDuff.Mode.SRC_ATOP);
             TextView timeTxt = (TextView) findViewById(R.id.editTimeFor);
             TimePickerDialog timePickerDialog = new TimePickerDialog
-                    (this,
-                    new TimePickerDialog.OnTimeSetListener() {
-                        @Override
-                        public void onTimeSet(TimePicker view, int hourOfDay,
-                                              int minute) {
-
-                            timeTxt.setText(hourOfDay + ":" + minute);
-                        }
-                    },
-                            mHour, mMinute, false);
+                    (this, new TimePickerDialog.OnTimeSetListener() {
+                                @Override
+                                public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                    timeTxt.setText(hourOfDay + ":" + minute);
+                                }
+                            }, mHour, mMinute, false);
+            
             timePickerDialog.show();
-       }
-       else{
+            
+        } else {
             System.out.println("Error onClickListener");
         }
-
     }
 
-    public void submitPost(View v){
+    public void submitPost(View v) {
         Intent intent = new Intent(this, MainActivity.class);
 
         v.startAnimation(buttonClick);
         v.getBackground().setColorFilter(0x77000000, PorterDuff.Mode.SRC_ATOP);
 
-        EditText titleElement   = findViewById(R.id.editTitle);
+        EditText titleElement = findViewById(R.id.editTitle);
         EditText contentElement = findViewById(R.id.editContent);
         TextView dateForElement = findViewById(R.id.editDateFor);
         TextView timeForElement = findViewById(R.id.editTimeFor);
 
-        String title   = titleElement.getText().toString();
+        String title = titleElement.getText().toString();
         String content = contentElement.getText().toString();
         String dateFor = dateForElement.getText().toString();
         String timeFor = timeForElement.getText().toString();
 
         intent.putExtra(EXTRA_TITLE, title);
         intent.putExtra(EXTRA_CONTENT, content);
-        intent.putExtra(EXTRA_DATEFOR, dateFor+" "+timeFor);
+        intent.putExtra(EXTRA_DATEFOR, dateFor + " " + timeFor);
 
         String completeDate = dateFor + " " + timeFor;
 
         try {
             postViewModel.savePost(new Post(title, content, formatDate(new Date()), completeDate));
-        } catch (NullPointerException npe){
+        } catch (NullPointerException npe) {
             System.out.println("NPE on Post element");
             postViewModel.savePost(new Post("fail", "This post has been failed in assigning", "fail", "fail"));
         }
@@ -134,15 +131,14 @@ public class AddPostActivity extends AppCompatActivity implements View.OnClickLi
         startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
     }
 
-    private String formatDate(Date date){
+    private String formatDate(Date date) {
         return new SimpleDateFormat("yyyy-MM-dd").format(date);
     }
 
     //Inner view.
     public static class TimePickerFragment extends DialogFragment
             implements TimePickerDialog.OnTimeSetListener {
-
-        public TimePickerFragment(){
+        public TimePickerFragment() {
             onAttachFragment(this);
         }
 
